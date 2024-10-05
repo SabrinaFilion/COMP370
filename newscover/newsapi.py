1e3ce7997ac5d3131542f851706a33cdfb76c108
SabrinaFilion <sabri@ip-172-26-11-140.ca-central-1.compute.internal>
2024-10-05 19:01:08 +0000

    Added newsapi.py script

---
+++ newscover/newsapi.py
@@ -0,0 +1,14 @@
+import requests
+URL=https://newsapi.org/v2/everything
+#Function which queries the NewsAPI, returning a list of news articles in english,
+# containing the keyworfs and published in the last lookback days
+fetch_latest_news(api_key, news_keywords, lookback_days=10)
+    response = requests.get()
+
+
+
+
+
+
+if __name__ =="__main__":
+    fetch_latest_news(5fb62789f7bd4435b2ae79c344b009c8,)
