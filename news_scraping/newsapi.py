bf6b16bb1b02b8e650fb78e452b95b978b1d264b
SabrinaFilion <119058186+SabrinaFilion@users.noreply.github.com>
2024-11-28 19:38:36 -0500

    Update newsapi.py

--- news_scraping/newsapi.py
+++ news_scraping/newsapi.py
@@ -2,9 +2,10 @@ import requests
 from datetime import datetime,timedelta
 URL="https://newsapi.org/v2/everything"
 
-#Function that queries the NewsAPI, returning a list of news articles in english,
-# containing the keywords given and published in the last lookback days
-def fetch_latest_news(api_key, news_keywords, lookback_days=10):
+#Function that queries the NewsAPI, returning a list of news articles in English,
+# containing the keywords given, and published in the last lookback days
+def fetch_latest_news(api_key, news_keywords, lookback_days):
+    
     #Checking if the news keywords are present
     if(not news_keywords):
         raise ValueError("Please provide keywords")
@@ -13,7 +14,7 @@ def fetch_latest_news(api_key, news_keywords, lookback_days=10):
         if( not keyword.isalpha()):
             raise ValueError("Keywords must contain only alphabetical characters")
     
-    #Getting the requests with all appropriate parameters
+    #Getting the requests with all the appropriate parameters
     response = requests.get(URL,params={"q":news_keywords,
                                         "language":"en",
                                         "from":(datetime.now()-timedelta(days=lookback_days)),
@@ -21,9 +22,3 @@ def fetch_latest_news(api_key, news_keywords, lookback_days=10):
     data= response.json()
     return data
 
-
-
-if __name__ =="__main__":
-    
-    data=fetch_latest_news("5fb62789f7bd4435b2ae79c344b009c8",["money"],10)
-    print(data)
