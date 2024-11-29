c2f7951e74037611464a9e1bdebe82d634ccfd04
SabrinaFilion <119058186+SabrinaFilion@users.noreply.github.com>
2024-11-28 19:35:00 -0500

    Update newsapiTest.py

--- news_scraping/tests/newsapiTest.py
+++ news_scraping/tests/newsapiTest.py
@@ -4,6 +4,7 @@ import json
 from datetime import timedelta, datetime
 
 from newsapi import fetch_latest_news
+
 #Getting the api_key
 file_path = os.path.join(os.path.dirname(__file__), "test_secrets.json")
 with open(file_path,"r") as file:
@@ -11,18 +12,20 @@ with open(file_path,"r") as file:
     api_key = data['api_key']
    
 class NewsAPITestCase(unittest.TestCase):
-        #Getting the api_key
+        
+    #Getting the api_key
     file_path = os.path.join(os.path.dirname(__file__), "test_secrets.json")
     with open(file_path,"r") as file:
         data = json.load(file)
         api_key = data['api_key']
-    #Testing that our function raises an exception if we do not provide keywords
+    
+    #Testing that the function raises an exception if we do not provide keywords
     def test_fail_nokeywords(self):
         with self.assertRaises(ValueError):
             fetch_latest_news(self.api_key,[],lookback_days=10)
             
 
-    #Testing that our function respects the lookback days
+    #Testing that the function respects the lookback days
     def test_lookback_days_respected(self):
         data=fetch_latest_news(self.api_key,["health"],lookback_days=5)
         articles=data['articles']
@@ -31,7 +34,7 @@ class NewsAPITestCase(unittest.TestCase):
             self.assertGreaterEqual(publish_date,(datetime.now()-timedelta(days=5)).strftime("%Y-%m-%d"))
             
 
-    #Testing that the function raises an exception if the keywords contain non alphabetical characters    
+    #Testing that the function raises an exception if the keywords contain non-alphabetical characters    
     def test_fail_nonalpha_keyword(self):
         with self.assertRaises(ValueError):
             fetch_latest_news(self.api_key,"money$",lookback_days=10)
